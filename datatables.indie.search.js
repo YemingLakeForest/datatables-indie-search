97551229b05a0c7d7fb47ee9f8c5cfdad026635d
YemingLakeForest <ansonn2005@hotmail.com>
2015-07-15 00:02:14 +0100

    Reformat coding with Brackets beautifier.

--- datatables.indie.search.js
+++ datatables.indie.search.js
@@ -1,16 +1,16 @@
-$.fn.dataTable.Api.register( 'indieSearch()', function (appliedCols) {
+$.fn.dataTable.Api.register('indieSearch()', function (appliedCols) {
 
-    this.columns(appliedCols).footer().each( function () {
-        var title = $('.indie-text-search').eq( $(this).index() ).text();
-        $(this).html( '<input type="text" placeholder="Search '+title+'" />' );
+    this.columns(appliedCols).footer().each(function () {
+        var title = $('.indie-text-search').eq($(this).index()).text();
+        $(this).html('<input type="text" placeholder="Search ' + title + '" />');
     });
-    
-    this.columns(appliedCols).every( function () {
+
+    this.columns(appliedCols).every(function () {
         var that = this;
- 
-        $( 'input', this.footer() ).on( 'keyup change', function () {
+
+        $('input', this.footer()).on('keyup change', function () {
             that
-                .search( this.value )
+                .search(this.value)
                 .draw();
         });
     });
